1a79dfdff3b5a8ac670d04aadef004bee0f98079
Sukhyun Baik <neonagx@gmail.com>
2016-12-12 11:25:15 -0800

    adds create party event tab with form creating on site

--- js/app.js
+++ js/app.js
@@ -14,6 +14,12 @@
         controller: 'ProfPartsController',
         controllerAs: 'profVm'
       })
+      .state('new', {
+        url: '/new',
+        templateUrl: 'new.html',
+        controller: 'ProfPartsNewController',
+        controllerAs: 'newVm'
+      })
       .state('signin', {
         url: '/signin',
         templateUrl: 'signin.html',
--- js/profsController.js
+++ js/profsController.js
@@ -2,8 +2,10 @@
   'use strict';
   angular.module('ThePartiest')
     .controller('ProfPartsController', ProfPartsController)
+    .controller('ProfPartsNewController', ProfPartsNewController)
 
   ProfPartsController.$inject = ['$http']
+  ProfPartsNewController.$inject = ['$http', '$state']
 
   function ProfPartsController($http){
     var vm = this
@@ -27,6 +29,21 @@
           var index = vm.all.indexOf(professional)
           vm.all.splice(index, 1)
         })
+    }
+  }
+
+  function ProfPartsNewController($http, $state){
+    var vm = this
+    vm.addProfPart = addProfPart
+    vm.newProfPart = {}
+
+    function addProfPart(){
+      $http
+        .post('http://localhost:3000/professionals', vm.newProfPart)
+        .then(function(res){
+          $state.go('index')
+        })
+        vm.newProfPart = {}
     }
   }
 }());
