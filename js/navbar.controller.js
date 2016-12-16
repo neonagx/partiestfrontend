ae9522a08db63626daef8465de4230b3450117ee
Sukhyun Baik <neonagx@gmail.com>
2016-12-15 17:15:52 -0800

    adds dialog and fixes several issues

--- js/navbar.controller.js
+++ js/navbar.controller.js
@@ -5,12 +5,26 @@
     .module("ThePartiest")
     .controller("NavbarController", NavbarController);
 
-  NavbarController.$inject = ["$log", "authService"];
+  NavbarController.$inject = ["$log", "authService", "$mdDialog"];
 
-  function NavbarController($log, authService) {
+  function NavbarController($log, authService, $mdDialog) {
     var vm = this;
 
     vm.authService = authService;
+    vm.showAlert = showAlert
+
+    function showAlert(){
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+         .clickOutsideToClose(true)
+         .title('The Partiest')
+         .textContent('The Partiest is a full MEAN Stack application that lets you create your own parties for your company.  Without using those boring Microsoft Office or powerpoint, it let you post relevant party information along with pictures and youtube videos to advertise for your sponsors.  With a built in Google API, the address for your party will be directed to you whenever you need!! SO COOL')
+         .ariaLabel('NOW GO OUT THERE AND MAKE SOME PARTY or PARTIES!!')
+         .ok('Got it!')
+         .targetEvent()
+       );
+    }
 
     $log.info("NavbarController loaded!");
   }
